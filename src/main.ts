0ecbe4caa33554f53f84f8c6ddaaccd2dd1e5034
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2022-05-24 17:26:17 +0200

    CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from "./app.module"
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
 
+    app.enableCors()
     const config = new DocumentBuilder()
         .setTitle("Textige server")
         .setDescription("The textige server API description")
